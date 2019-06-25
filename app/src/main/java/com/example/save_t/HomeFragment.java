7f0f1fd886754cb14b99cc2c7899bbc7379d101e
Jvschaik <jsonschaik@gmail.com>
2019-06-25 14:57:32 +0200

    bottom nav fix

--- app/src/main/java/com/example/save_t/HomeFragment.java
+++ app/src/main/java/com/example/save_t/HomeFragment.java
@@ -15,6 +15,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+
 public class HomeFragment extends Fragment {
 
     private Button btnLiving;
--- app/src/main/java/com/example/save_t/MainActivity.java
+++ app/src/main/java/com/example/save_t/MainActivity.java
@@ -1,25 +1,15 @@
 package com.example.save_t;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.nfc.Tag;
-import android.os.Bundle;
 
-import android.util.AttributeSet;
+
+import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.GridView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -40,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "From: " + MyFirebaseMessagingService.getToken(this));
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener( MainActivity.this,  new OnSuccessListener<InstanceIdResult>() {
             @Override
